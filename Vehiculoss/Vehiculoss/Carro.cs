fde4f1a9a665e4085441f15801a263aafeebf5f3
MarialejandraRiascos <104032166+MarialejandraRiascos@users.noreply.github.com>
2022-05-22 23:40:23 -0500

    Add files via upload

---
+++ Vehiculoss/Vehiculoss/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vehiculoss
+{
+    public class Carro
+    {
+        private int nombreCarro;
+        private string tipoCarro;
+        private string tamaño;
+    }
+}
---
+++ Vehiculoss/Vehiculoss/Vehiculos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vehiculoss
+{
+    public class Vehiculos
+    {
+        private string nombreVehiculo;
+        private string tipoMotor;
+        private int cantidadPersonas;
+    }
+}
